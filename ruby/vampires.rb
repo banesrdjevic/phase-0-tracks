a907a37f04b3447ba039006512ecf5b1d031393c
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2016-10-07 13:36:57 -0500

    Add interview questions for vampires

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+
+puts "What is your name?"
+  name = gets.chomp
+puts "How old are you?"
+  age = gets.chomp
+puts "What year were you born?"
+  birth_yr = gets.chomp
+puts "Our company cafeteria serves garlic bread.  Would you like to try some?"
+  garlic = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+  insurance = gets.chomp
+

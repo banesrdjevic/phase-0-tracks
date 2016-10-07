211687c11a035769977b8a22562b45f4a5089173
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2016-10-07 18:54:08 -0500

    Add code to decrypt password

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -6,7 +6,17 @@
 # output: assume lowercase
 # spaces don't change
 
-password = "quazi modo"
+puts "Do you want encrypted or decrypted?"
+  choice = gets.chomp
+puts "What would you like to use for a password?"
+  password = gets.chomp  
+
+if choice == "encrypted"
+    puts encrypt(password)
+else
+    puts decrypt(password)
+end
+
 
 def encrypt(string_1)
   counter = 0
@@ -21,71 +31,103 @@ def encrypt(string_1)
   end
 end
 
-puts encrypt(password)
-
+# puts encrypt("quazi modo")
 
 # pseudo code decrypt
 # loop through password using [-1]
 # .length - 1
 # spaces remain the same
 
-
-# initialize the counter OUTSIDE the loop
-counter = 0
-
-while counter < 10
-  # print the counter
-  puts counter
-  
-  # avoid an infinite loop
-  counter += 1
+def decrypt(string_1)
+  counter = 0
+  while counter < string_1.length
+    if string_1[counter].index("z")
+      puts "y"
+      counter += 1
+    elsif string_1[counter].index("y")
+      puts "x"
+      counter += 1
+    elsif string_1[counter].index("x")
+      puts "w"
+      counter += 1
+    elsif string_1[counter].index("w")
+      puts "v"
+      counter += 1
+    elsif string_1[counter].index("v")
+      puts "u"
+      counter += 1
+    elsif string_1[counter].index("u")
+      puts "t"
+      counter += 1
+    elsif string_1[counter].index("t")
+      puts "s"
+      counter += 1
+    elsif string_1[counter].index("s")
+      puts "r"
+      counter += 1
+    elsif string_1[counter].index("r")
+      puts "q"
+      counter += 1
+    elsif string_1[counter].index("q")
+      puts "p"
+      counter += 1
+    elsif string_1[counter].index("p")
+      puts "o"
+      counter += 1
+    elsif string_1[counter].index("o")
+      puts "n"
+      counter += 1
+    elsif string_1[counter].index("n")
+      puts "m"
+      counter += 1
+    elsif string_1[counter].index("m")
+      puts "l"
+      counter += 1
+    elsif string_1[counter].index("l")
+      puts "k"
+      counter += 1
+    elsif string_1[counter].index("k")
+      puts "j"
+      counter += 1
+    elsif string_1[counter].index("j")
+      puts "i"
+      counter += 1
+    elsif string_1[counter].index("i")
+      puts "h"
+      counter += 1
+    elsif string_1[counter].index("h")
+      puts "g"
+      counter += 1
+    elsif string_1[counter].index("g")
+      puts "f"
+      counter += 1
+    elsif string_1[counter].index("f")
+      puts "e"
+      counter += 1
+    elsif string_1[counter].index("e")
+      puts "d"
+      counter += 1
+    elsif string_1[counter].index("d")
+      puts "c"
+      counter += 1
+    elsif string_1[counter].index("c")
+      puts "b"
+      counter += 1
+    elsif string_1[counter].index("b")
+      puts "a"
+      counter += 1
+    else string_1[counter].index(" ")
+      puts " "
+      counter += 1
+    end
+  end
 end
 
-index = 0
-secret_password = "unicorn"
+# puts decrypt("txpse gjti")
 
-greeting = "hello"
+# puts encrypt("abc")
+# puts encrypt("zed")
+# puts decrypt("bcd")
+# puts decrypt("afe")
 
-# use the index to print
-# each letter
-p greeting[0]
-p greeting[1]
-p greeting[2]
-p greeting[3]
-p greeting[4]
-
-# or reverse it
-p greeting[-1]
-p greeting[-2]
-p greeting[-3]
-p greeting[-4]
-p greeting[-5]
-
-
-# strings are zero-indexed,
-# so .length on a string will always
-# return 1 more than the largest index.
-# If length is 7, the index of the 
-# last letter is 6.
-
-puts "Length of password is #{secret_password.length} letters."
-
-puts "The letter at index 6 is #{secret_password[6]}."
-
-# let's use .length to loop 
-# through the word
-# and simply print each letter ...
-while index < secret_password.length
-  p secret_password[index]
-  index += 1
-end
-
-# or loop through and do something
-# interesting with each letter
-index = 0
-while index < secret_password.length
-  secret_password[index] = secret_password[index].upcase!
-  index += 1
-end
-
-puts secret_password
+# puts decrypt(encrypt("swordfish"))

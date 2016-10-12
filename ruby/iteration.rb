35f2821493f6372b2a13a2585656c7561afedcf2
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2016-10-12 18:49:27 -0500

    Add each and map examples to code for release one

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,3 +5,37 @@ def iteration_status
 end
 
 iteration_status { |x| puts "this block has run #{x}."}
+
+# Birds Array
+
+birds = [
+  "Robin",
+  "Bluejay",
+  "Woodpecker",
+  "Turkey"
+]
+
+birds.each do |the_bird|
+  puts "#{the_bird} is a bird."
+end
+
+birds.map! do |the_bird|
+  the_bird.upcase
+end
+
+p birds
+
+
+# Animals Hash
+
+animals = {
+  "racoon" => "grey",
+  "squirrel" => "grey",
+  "frog" => "green",
+  "deer" => "brown",
+  "opossum" => "white"
+}
+
+animals.each do |animal, color|
+  puts "the #{animal} is #{color}."
+end

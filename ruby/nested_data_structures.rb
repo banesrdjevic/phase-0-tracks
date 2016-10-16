99ddce9ee3022f8abfb151c7087fdc5b715d8b56
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2016-10-16 19:47:19 -0400

    Outline layout for data structures

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,9 @@
+# Nested Data Structures
+
+# Build a 6 apartment complex
+
+# Lobby, 3 chairs, table w/ magazines, mirror
+# Main Office, 
+# Apt 1, Dog, 1 bedroom, 1 bath,
+# Apt 2, Cat, 2 bedroom, 1 bath,
+# Apt 3, 2 birds, studio, patio

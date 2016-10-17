ea13bcaabad95cfde516dcbc83b93ae6340cf6b5
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2016-10-17 01:00:55 -0500

    Add nested data describing apartments'

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,9 +1,52 @@
 # Nested Data Structures
 
-# Build a 6 apartment complex
+# Build a 3 apartment complex
 
 # Lobby, 3 chairs, table w/ magazines, mirror
-# Main Office, 
-# Apt 1, Dog, 1 bedroom, 1 bath,
-# Apt 2, Cat, 2 bedroom, 1 bath,
-# Apt 3, 2 birds, studio, patio
+# Apt 1, Dog, Cat, 1 bedroom, 1 bath,
+# Apt 2, Cat, Lizard, 2 bedroom, 1 bath,
+# Apt 3, 2 birds, 3 tarantulas, 3 bedroom, 2 and a half bath, patio
+
+
+apartment_complex = {
+  'lobby_items': [
+    'chairs'
+    'table'
+    'magazine'
+    'mirror'
+  ]
+  'apt_1': {
+    'layout': {
+      Bedrooms: 1,
+      Bath: 1
+    }
+    'pets': [
+      'dog',
+      'cat'
+    ]
+  }
+  'apt_2': {
+    'layout': {
+      Bedrooms: 2,
+      Bath: 1
+    }
+    'pets': [
+      'cat',
+      'lizard'
+    ]
+  }
+  'apt_3': {
+    'pets_3': [
+      'bird', 
+      'bird',
+      'tarantula',
+      'tarantula',
+      'tarantula'
+    ]
+    'layout_3': {
+      Bedrooms: 3,
+      Bath: 2.5,
+      Patio: 1
+    }
+  }
+}

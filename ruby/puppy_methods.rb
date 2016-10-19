481fa9e832c6ae448783b11ff319e1caf0b04b66
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2016-10-19 17:17:47 -0500

    Add code for new class, loop and store data structure

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -42,3 +42,54 @@ Ellie.dog_years(10)
 
 Ellie.shake
 
+
+# In puppy_methods.rb, design and implement your own class below the Puppy class 
+# -- anything you'd like, but it should have an initialize method and at least two other instance methods.
+# Then do the following:
+
+class Car
+
+  def initialize
+    p "Initializing new car instance ..."
+  end
+
+  def turn_signals (direction)
+    if direction == "left"
+      puts "*left signal on*"
+    else direction == 'right'
+      puts '*right signal on*'
+    end
+  end
+
+  def car_horn
+    puts "Beep beep!"
+  end
+end
+
+car_database = []
+
+
+index = 0
+
+# car_count = 1
+
+until index >= 50
+
+  car_instance = Car.new
+
+  # car_instance = "Car Number #{car_count}"
+
+  # car_count += 1
+
+  car_database << car_instance
+
+  index +=1
+end
+
+puts car_database
+
+car_database.each do |car|
+  puts car.turn_signals("left")
+  puts car.car_horn
+end
+

32c4485953c5d492b7f98d8a657137968229ff93
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2016-10-22 13:35:59 -0500

    Add code for celebrate birthday, get mad at, and setter method for @gender.

--- ruby/reindeer.rb
+++ ruby/reindeer.rb
@@ -1,6 +1,6 @@
 class Reindeer
-  attr_reader :location
-  attr_accessor :name
+  # attr_reader :location
+  # attr_accessor :name # without this, line 53 won't work
 
   def initialize(name)
     @name = name
@@ -16,11 +16,47 @@ class Reindeer
     puts "#{@name} landed safely in #{location}."
     @location = location
   end
+
+  def about
+    puts "Name: #{@name}"
+    puts "Location: #{@location}"
+  end
+
+# getter methods making priv
+  def name # allows line 50 and 54 to function
+    @name
+  end
+
+  def location # allows line 50 and 54 to function
+    @location
+  end
+
+# setter methods
+  def name=(new_name)
+    @name = new_name
+  end
+  
 end
 
+# Way shown in video on 6.3, called about attribute instead of using getter and setter methods
+reindeer = Reindeer.new("Blitzen")
+reindeer.about
+reindeer.take_off(3000)
+reindeer.land("Bombay")
+reindeer.about
+
+reindeer = Reindeer.new("Rudolph")
+reindeer.about
+reindeer.take_off(3000)
+reindeer.land("Paris")
+reindeer.about
+
+# Second video in 6.3 allows for use of setter and getter videos...
 reindeer = Reindeer.new("Dasher")
 puts "#{reindeer.name} is in #{reindeer.location}."
 reindeer.take_off(3000)
 reindeer.land("Cape Town")
 reindeer.name = "The Reindeer Formerly Known as Dasher"
 puts "#{reindeer.name} is in #{reindeer.location}."
+
+
--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,8 +4,11 @@
 # Santa class
 class Santa
   # initialize method that prints "Initializing Santa instance ..."
-  def initialize
-    p "Initializing Santa instance ..."
+  def initialize(gender, ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+    @age = 0
+    p "Initializing Santa instance ... #{@gender}, #{@ethnicity}, #{@age}"
   end
 
   # speak method that will print "Ho, ho, ho! Haaaappy holidays!"
@@ -19,14 +22,64 @@ class Santa
     puts "That was a good #{cookie_type} cookie!"
   end
 
+  # celebrate birthday, should add 1 year to birthday
+  def celebrate_birthday
+    @age += 1
+  end
+
+  # get_mad_at should change the order of the reindeer array
+  # ---------------------------
+  def get_mad_at(reindeer_name)
+    #Reindeer ranking array most preferred to least preferred
+    reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    # Reassign Reindeer to last place
+    reindeer_ranking.delete(reindeer_name)
+    reindeer_ranking.push(reindeer_name)
+    puts reindeer_ranking
+  end
+
+  # setter method for @gender
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
 end
 
 # DRIVER CODE / TEST CODE LOGIC
 
-Clause = Santa.new
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
 
+puts "The following santas are now included..."
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+Clause = Santa.new("male", "white")
 Clause.class
-
 Clause.speak
-
 Clause.eat_milk_and_cookies('chocolate chip')
+Clause.celebrate_birthday
+Clause.get_mad_at("Vixen")
+
+# See reindeer.rb for more info
+
+# class Reindeer
+  # def initialize(name)
+    # @name = name
+  # end
+# end
+
+# reindeer = Reindeer.new("Blitzen")
+# reindeer.name
+

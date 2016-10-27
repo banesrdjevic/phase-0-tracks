6c4df10be56068db87047db9d48e7a70d3afaa31
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2016-10-27 16:06:16 -0500

    add house code functionality to call string'

--- ruby/house.rb
+++ ruby/house.rb
@@ -8,9 +8,71 @@
 # Methods:
 # - getter for rooms
 # - add_room (only allows up to 10 rooms)
-# - square_footage (adds upp the house's square footage and returns it as an integer)
+# - square_footage (adds up the house's square footage and returns it as an integer)
 # - total_value (adds up value of items in all rooms)
 # - to_s override
+require_relative "room"
+require_relative "item" # this doesn't appear to be necessary as room file requires the item file.
+
+class House
+  attr_reader :rooms
+
+  def initialize
+    @rooms = []
+  end
+
+  def add_room(room)
+    if @rooms.length < 11
+      @rooms << room
+      true # this returns true so that driver code can process whether or not this actually happened.
+    else
+      # puts "Cannot add room."
+      false # return false instead of printing something so that driver code can then interpret without user having to see it.
+    end
+  end
+
+  def to_s
+      house_str = ""
+      @rooms.each do |room|
+        house_str << room.to_s.upcase
+        house_str << "\n\n"
+        room.items.each do |item|
+          house_str << item.to_s
+          house_str << "\n"
+        end
+        house_str << "\n"
+      end
+      house_str
+  end
+
+end
+
+
+#DRIVER CODE
+# Make House
+house = House.new
+
+# Make living room
+living_room = Room.new("Living room", 20, 35)
+# puts living_room
+piano = Item.new("Piano", "black", 10000)
+box = Item.new("cardboard box", "brown", 0)
+living_room.items << piano
+living_room.items << box
+# puts living_room.total_value
+
+# Make kitchen
+kitchen = Room.new("Kitchen", 10, 15)
+sink = Item.new("sink", "white", 5000)
+oven = Item.new("over", "black", 3000)
+kitchen.items << sink
+kitchen.items << oven
+
+house.add_room(living_room)
+house.add_room(kitchen)
+
+puts house
+
 
 # ROOM CLASS
 # Attributes:

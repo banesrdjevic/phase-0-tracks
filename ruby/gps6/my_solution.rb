04e476661fa129d757f487cd71684e1644cc1cf4
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2016-10-26 19:42:10 -0500

    Add comments for release 8

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,7 +1,7 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [by myself, with: Dennis Marchetti].
+# We spent [3] hours on this challenge.
 
 # EXPLANATION OF require_relative
 # Telling the program that you need the data inputs from the state_data.rb file.

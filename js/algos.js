c49e00ad0fc6d3bcf44306ad2d049ff073fbb43b
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2016-10-31 17:10:40 -0500

    Played around with 7.3 some more trying to figure out release 2 and 3

--- js/algos.js
+++ js/algos.js
@@ -4,10 +4,9 @@
 // Write a function to take an array of words and return the longest word or phrase.
 // Add driver code to test
 
-// var phrases_list = ["this", "another phrase", "still even longer phrase", "shorter", "the longest phrase ever continues", "short"]
-
-function longest_phrase() {
-  var phrases = ["this", "another phrase", "still even longer phrase", "shorter", "the longest phrase ever continues", "short"];
+// BUSINESS LOGIC
+function longest_phrase(phrases) {
+  // var phrases = ["this", "another phrase", "still even longer phrase", "shorter", "the longest phrase ever continues", "short"];
   var winning_length = "";
   for (var i = 0; i < phrases.length; i++) {
     if (phrases[i].length > winning_length.length) {
@@ -18,12 +17,33 @@ function longest_phrase() {
   console.log(winning_length)
 }
 
-longest_phrase()
+// DRIVER CODE
+var phrases_list = ["this", "another phrase", "still even longer phrase", "shorter", "the longest phrase ever continues", "short"]
+
+longest_phrase(phrases_list)
 // console.log(winning_length)
 
 // RELEASE 1
 // write a function that takes two objects and checks to see if the objects share at least one key-value pair. 
 
+var sky = {clouds: true, color: "blue", windspeed: 10, distance: "endless"};
+var jessica = {hair: "blonde", age: 34, color: "blue", height: "5 ft 4"};
+
+while (var i = 0; i < sky.length; i++) {
+  for (var key in sky) {
+    if (sky[key] in jessica) {
+      for 
+    }
+  }
+}
 
 // RELEASE 2
 // Write a function that takes an integer for length, and builds and returns an array of strings of the given length.
+
+function build_array(x) {
+  var str = "";
+  var array = [];
+  while (var i = 0; i < x; i++) {
+    array.push(str)
+  }
+}
